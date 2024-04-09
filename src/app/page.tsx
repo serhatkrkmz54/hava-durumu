22b8206bd82713e209554dd70b9b1ea9bb537313
serhatkrkmz54 <korkmaz_serhat@hotmail.com>
2024-04-10 01:06:59 +0300

    SON-DEGISIKLIK

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,97 @@
+'use client'
 import Image from "next/image";
 import Navbar from "./componentler/Navbar";
 import Arama from "./componentler/Arama";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface Clouds {
+  all: number;
+}
+
+interface Wind {
+  speed: number;
+  deg: number;
+  gust: number;
+}
+
+interface Main {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  sea_level: number;
+  grnd_level: number;
+  humidity: number;
+  temp_kf: number;
+}
+
+interface Sys {
+  pod: string;
+}
+
+interface City {
+  id: number;
+  name: string;
+  coord: {
+    lat: number;
+    lon: number;
+  };
+  country: string;
+  population: number;
+  timezone: number;
+  sunrise: number;
+  sunset: number;
+}
+
+interface ForecastItem {
+  dt: number;
+  main: Main;
+  weather: Weather[];
+  clouds: Clouds;
+  wind: Wind;
+  visibility: number;
+  pop: number;
+  sys: Sys;
+  dt_txt: string;
+}
+
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: ForecastItem[];
+  city: City;
+}
 
 export default function Home() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: async () =>{
+      const {data} =await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=sakarya&appid=${process.env.NODE_ENV}&cnt=2`);
+      return data;
+    }
+
+  },)
+
+  console.log("data => ",data);
+
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar/>
-      <Arama />
     </div>
   );
 }
